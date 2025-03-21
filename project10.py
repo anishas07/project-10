c98bf5a6e3c00296e3cc7a99edc7d3777f731dc3
anishas07 <Studytea95@gmail.com>
2025-03-21 20:18:32 +1030

    Add files via upload

---
+++ project10.py
@@ -0,0 +1,24 @@
+class Math:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.lendDict = {}
+
+    def display(self):  
+        total = self.x + self.y
+        print("x + y =", total)
+
+ob = Math(10, 5)
+
+ob.display()
+
+#destructor
+def __del__(self):
+        print("Destructor was used.")
+
+    
+
+   
+        
+
+    
